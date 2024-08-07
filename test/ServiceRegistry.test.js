9e20c5c2d604d7fe91712104fb5725420ba1b724
Ekrem Seren <ekrem.seren@gmail.com>
2024-08-07 16:31:50 +0300

    add more tests

--- test/ServiceRegistry.test.js
+++ test/ServiceRegistry.test.js
@@ -41,5 +41,69 @@ describe("ServiceRegistry", function () {
             await expect(await cmAccountManager.getRegisteredServiceHashByName(serviceName)).to.be.equal(serviceHash);
             await expect(await cmAccountManager.getRegisteredServiceNameByHash(serviceHash)).to.be.equal(serviceName);
         });
+
+        it("should unregister a service correctly", async function () {
+            const { cmAccountManager } = await loadFixture(deployAndConfigureAllFixture);
+
+            const SERVICE_REGISTRY_ADMIN_ROLE = await cmAccountManager.SERVICE_REGISTRY_ADMIN_ROLE();
+
+            // Grant SERVICE_REGISTRY_ADMIN_ROLE
+            await expect(
+                cmAccountManager
+                    .connect(signers.managerAdmin)
+                    .grantRole(SERVICE_REGISTRY_ADMIN_ROLE, signers.otherAccount1.address),
+            )
+                .to.emit(cmAccountManager, "RoleGranted")
+                .withArgs(SERVICE_REGISTRY_ADMIN_ROLE, signers.otherAccount1.address, signers.managerAdmin.address);
+
+            const serviceName = "cmp.service.accommodation.v1alpha.AccommodationSearchService";
+            const serviceHash = ethers.keccak256(ethers.toUtf8Bytes(serviceName));
+
+            await expect(cmAccountManager.connect(signers.otherAccount1).registerService(serviceName))
+                .to.emit(cmAccountManager, "ServiceRegistered")
+                .withArgs(serviceName, serviceHash);
+
+            await expect(cmAccountManager.connect(signers.otherAccount1).unregisterService(serviceName))
+                .to.emit(cmAccountManager, "ServiceUnregistered")
+                .withArgs(serviceName, serviceHash);
+        });
+
+        it("should revert if the service is already registered", async function () {
+            const { cmAccountManager } = await loadFixture(deployAndConfigureAllFixture);
+
+            const SERVICE_REGISTRY_ADMIN_ROLE = await cmAccountManager.SERVICE_REGISTRY_ADMIN_ROLE();
+
+            // Grant SERVICE_REGISTRY_ADMIN_ROLE
+            await expect(
+                cmAccountManager
+                    .connect(signers.managerAdmin)
+                    .grantRole(SERVICE_REGISTRY_ADMIN_ROLE, signers.otherAccount1.address),
+            )
+                .to.emit(cmAccountManager, "RoleGranted")
+                .withArgs(SERVICE_REGISTRY_ADMIN_ROLE, signers.otherAccount1.address, signers.managerAdmin.address);
+
+            const serviceName = "cmp.service.accommodation.v1alpha.AccommodationSearchService";
+
+            // Register the service
+            await expect(cmAccountManager.connect(signers.otherAccount1).registerService(serviceName)).to.be.not
+                .reverted;
+
+            // Register the service again
+            await expect(cmAccountManager.connect(signers.otherAccount1).registerService(serviceName))
+                .to.be.revertedWithCustomError(cmAccountManager, "ServiceAlreadyRegistered")
+                .withArgs(serviceName);
+        });
+
+        it("should revert if the caller does not have the SERVICE_REGISTRY_ADMIN_ROLE", async function () {
+            const { cmAccountManager } = await loadFixture(deployAndConfigureAllFixture);
+
+            const SERVICE_REGISTRY_ADMIN_ROLE = await cmAccountManager.SERVICE_REGISTRY_ADMIN_ROLE();
+
+            const serviceName = "cmp.service.accommodation.v1alpha.AccommodationSearchService";
+
+            await expect(cmAccountManager.connect(signers.otherAccount1).registerService(serviceName))
+                .to.be.revertedWithCustomError(cmAccountManager, "AccessControlUnauthorizedAccount")
+                .withArgs(signers.otherAccount1.address, SERVICE_REGISTRY_ADMIN_ROLE);
+        });
     });
 });
