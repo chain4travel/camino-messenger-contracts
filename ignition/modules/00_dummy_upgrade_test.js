39d2879ddfa1079322684ae200f4af0bc329b5ca
Ekrem Seren <ekrem.seren@gmail.com>
2024-09-02 23:02:08 +0300

    rename and fix upgrade test

--- ignition/modules/00_dummy_upgrade_test.js
+++ ignition/modules/00_dummy_upgrade_test.js
@@ -1,11 +1,11 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { vars } = require("hardhat/config");
-const CMAccountManagerModule = require("./CMAccountManager");
+const CaminoMessengerModule = require("./messenger");
 
 // Simple upgrade test
 
 const upgradeModule = buildModule("UpgradeModule", (m) => {
-    const { managerProxy } = m.useModule(CMAccountManagerModule);
+    const { managerProxy } = m.useModule(CaminoMessengerModule);
 
     const cmAccountManagerV2 = m.contract("CMAccountManagerV2");
 
