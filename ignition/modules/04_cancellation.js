f2a6ca36086e148d9e23f8c67c20e5751bbcfd7d
Ekrem Seren <ekrem.seren@gmail.com>
2024-10-24 05:20:12 +0300

    add cancellation module

---
+++ ignition/modules/04_cancellation.js
@@ -0,0 +1,40 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const CaminoMessengerModule = require("./01_messenger");
+
+// Upgrades for the BookingToken Cancellation support
+const CancellationModule = buildModule("CancellationModule", (m) => {
+    // Use module to get the managerProxy and bookingTokenProxy
+    const { managerProxy, bookingTokenProxy } = m.useModule(CaminoMessengerModule);
+
+    // BookingTokenOperator is updated to support Cancellation. We need to deploy a
+    // new library for CMAccount implementation.
+    const bookingTokenOperator = m.library("BookingTokenOperator");
+
+    // Deploy a new CMAccount implementation with the new BookingTokenOperator
+    // library. There are also updates to the CMAccount impl contract to support
+    // cancellation functions like initiate/accept/reject cancellation.
+    const newCMAccountImpl = m.contract("CMAccount", [], {
+        libraries: { BookingTokenOperator: bookingTokenOperator },
+    });
+
+    // Set the new CMAccount implementation in the manager
+    m.call(managerProxy, "setAccountImplementation", [newCMAccountImpl], {
+        id: "NewCMAccountImpl",
+    });
+
+    /*********************************************
+     *        Upgrade BookingToken to V2         *
+     *********************************************/
+
+    // Deploy the BookingTokenV2 contract
+    const bookingTokenV2 = m.contract("BookingTokenV2", [], {
+        id: "BookingTokenV2Impl",
+    });
+
+    // Upgrade the BookingToken contract to V2
+    m.call(bookingTokenProxy, "upgradeToAndCall", [bookingTokenV2, "0x"]);
+
+    return { managerProxy, bookingTokenProxy, bookingTokenOperator, newCMAccountImpl };
+});
+
+module.exports = CancellationModule;
